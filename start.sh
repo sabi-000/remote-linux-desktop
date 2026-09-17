ab41f68cabbe14dde3ba87f8251ee382e101a5a5
Saheb Karami <sahebkarami@gmail.com>
2026-09-17 22:55:25 +0330

    Allow insecure VNC mode behind noVNC proxy

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ mkdir -p /root/.vnc
 printf '#!/bin/sh\nstartxfce4 &\n' > /root/.vnc/xstartup
 chmod 0755 /root/.vnc/xstartup
 
-vncserver "${DISPLAY}" -localhost no -SecurityTypes None -geometry "${GEOMETRY}" -depth 24
+vncserver "${DISPLAY}" -localhost no -SecurityTypes None -geometry "${GEOMETRY}" -depth 24 --I-KNOW-THIS-IS-INSECURE
 exec websockify --web=/usr/share/novnc/ "0.0.0.0:${PORT}" "localhost:$((5900 + DISPLAY_NUM))"
